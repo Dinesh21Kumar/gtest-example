46b0a2d5d3785615b4088d53cf50ae1469acd344
Dinesh Kumar <dinesh21.kumar@ril.com>
2020-01-22 23:06:46 +0530

    first commit

---
+++ build/googletest-build/googlemock/gtest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/dineshkumar/Documents/jiocloud/testy/build/googletest-src/googletest
+# Build directory: /Users/dineshkumar/Documents/jiocloud/testy/build/googletest-build/googlemock/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ build/test/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /Users/dineshkumar/Documents/jiocloud/testy/test
+# Build directory: /Users/dineshkumar/Documents/jiocloud/testy/build/test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(customer "/Users/dineshkumar/Documents/jiocloud/testy/build/test/customer_tests")
+set_tests_properties(customer PROPERTIES  _BACKTRACE_TRIPLES "/Users/dineshkumar/Documents/jiocloud/testy/test/CMakeLists.txt;17;add_test;/Users/dineshkumar/Documents/jiocloud/testy/test/CMakeLists.txt;0;")
+add_test(widget "/Users/dineshkumar/Documents/jiocloud/testy/build/test/widget_tests")
+set_tests_properties(widget PROPERTIES  _BACKTRACE_TRIPLES "/Users/dineshkumar/Documents/jiocloud/testy/test/CMakeLists.txt;17;add_test;/Users/dineshkumar/Documents/jiocloud/testy/test/CMakeLists.txt;0;")
